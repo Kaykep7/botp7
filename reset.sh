7a1c846672e8896015ce9ca88a7f9496a1b39bf6
gusta4321 <gustavonunes.p@gmail.com>
2022-02-11 12:48:35 -0300

    p7 2.1

---
+++ reset.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+GREEN='\033[0;31m'
+GRE='\033[0;32m'
+
+
+echo "${GREEN} Resetando."
+sleep 1
+echo "${GREEN} Resetando.."
+sleep 1
+echo "${GREEN} Resetando..."
+
+
+rm -rf BarBar.json
+
+sleep 1
+
+echo "${GRE} QR CODE RESETADO COM SUCESSO"
+
